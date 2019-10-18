4b3dec162668d16ff022e622f827e6423815c3ba
Cloud Cowboy <50215787+tmrayer@users.noreply.github.com>
2019-10-18 11:02:45 -0500

    Add files via upload

---
+++ Get-DomainControllersList.ps1
@@ -0,0 +1,6 @@
+﻿### Query Active Directory Active User Accounts
+## Written by the Cloud Cowboy
+## Pop up requests for you to enter information
+$Domain = Read-Host 'Enter the FQDN of the Domain you want to query'
+$username = Read-Host 'Enter the username in the Domain you want to query'
+Get-ADDomainController -Filter * -Server $Domain -Credential (Get-Credential $Domain\$username) | Select Name, ipv4Address, OperatingSystem, site | Sort-Object -Property Name >C:\temp\domainlist\"$domain"DClist.txt 
